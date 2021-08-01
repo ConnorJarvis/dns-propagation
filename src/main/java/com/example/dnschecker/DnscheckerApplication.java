931dfc3c14726fafec050c0013bcb3bdf1119003
Connor Jarvis <connorpjarvis@gmail.com>
2021-08-01 13:24:56 -0400

    Init

---
+++ src/main/java/com/example/dnschecker/DnscheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnschecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnscheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnscheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dnschecker/DnscheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dnschecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnscheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
